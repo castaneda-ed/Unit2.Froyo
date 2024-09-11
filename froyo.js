e5d992b9929674f41de15f5eac6c0e3ff5754c1e
Edwin <edwincastaneda0801@gmail.com>
2024-09-11 16:07:12 -0500

    compacted two functions into only one and finished

--- froyo.js
+++ froyo.js
@@ -5,14 +5,16 @@ When they view the browser console, they observe a table listing how many
 of each flavor they have ordered. In this case, they will be able to observe 
 that they have ordered three vanilla, two coffee, and one strawberry froyo.  */
 
-const userImput = prompt('Tell me what froyo flavor do you want separated by commas', 
-     'vanilla,vanilla,vanilla,strawberry,coffee,coffee');
+//Ask the users for flavors as input
+const userImput = prompt('Type flavors (vanilla, coffee, strawberry, chocolate) separated by commas', 
+     'vanilla,vanilla,vanilla,strawberry,coffee,coffee,chocolate');
 
+// transforms user input to an array of strings
 const flavorsArray = userImput.split(',')
 
 console.log(flavorsArray)
 
-/**
+/**Function to iterate through users flavor and turn them in to an object
  * @param {string[]} flavors an array with the custumer's flavor
  * @returns {object} an object with the flavors keys and quantities values
  */
@@ -24,33 +26,43 @@ function createObject(flavors) {
      }
      for(let flavor of flavors){
      if(flavor == 'vanilla'){
-          customerFlavors.vanilla += +1;
+          customerFlavors.vanilla += 1;
      }
      else if(flavor == 'coffee'){
-          customerFlavors.coffee += +1;
+          customerFlavors.coffee += 1;
      }
      else if(flavor == 'strawberry'){
-          customerFlavors.strawberry += +1;
+          customerFlavors.strawberry += 1;
+     }
+     else if(flavor == 'chocolate'){
+          customerFlavors.chocolate += 1
      }
 }
      return customerFlavors
 }
 
 console.log(createObject(flavorsArray))
-function giveFlavorsQuantity(flavors, quantities) {
-     let sum = quantities;
-     for(const flavor of flavors) {
-          if(flavor == 'vanilla'){
-               sum.vanilla += +1;
-          }
-          else if(flavor == 'coffee'){
-               sum.coffee += +1;
-          }
-          else if(flavor == 'strawberry'){
-               sum.strawberry += +1;
-          }
-     }
+
+const tableOfQuantities = createObject(flavorsArray)
+
+console.log(tableOfQuantities)
+
+
+// THIS WAS WORKING BUT DISCOVERED THAT I WAS ABLE  TO DO IT IN UN FUNCTION
+// function giveFlavorsQuantity(flavors, quantities) {
+//      let sum = quantities;
+//      for(const flavor of flavors) {
+//           if(flavor == 'vanilla'){
+//                sum.vanilla += +1;
+//           }
+//           else if(flavor == 'coffee'){
+//                sum.coffee += +1;
+//           }
+//           else if(flavor == 'strawberry'){
+//                sum.strawberry += +1;
+//           }
+//      }
  
-     return sum
-}
- console.log(giveFlavorsQuantity(flavorsArray, createObject(flavorsArray)))
+//      return sum
+// }
+//  console.log(giveFlavorsQuantity(flavorsArray, createObject(flavorsArray)))
